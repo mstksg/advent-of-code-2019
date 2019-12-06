82d35a6f5884faacdd4ace7eb80118a0fcfc3b9f
Justin Le <justin@jle.im>
2019-12-06 00:57:20 -0800

    some more comments

--- src/AOC/Challenge/Day05.hs
+++ src/AOC/Challenge/Day05.hs
@@ -15,27 +15,23 @@ module AOC.Challenge.Day05 (
   , fillModes
   ) where
 
-import           AOC.Common                (loopMaybeM)
-import           AOC.Solver                ((:~>)(..))
-import           AOC.Util                  (maybeToEither, eitherToMaybe)
-import           Control.Applicative
-import           Control.Monad             (void)
-import           Control.Monad.Except      (MonadError(..))
-import           Control.Monad.Loops
-import           Control.Monad.State
-import           Control.Monad.Trans.Maybe
-import           Data.Conduit              (ConduitT, await, yield, (.|), runConduit)
-import           Data.Conduit.Lift
-import           Data.List.Split           (splitOn)
-import           Data.Map                  (Map)
-import           Data.Sequence             (Seq)
-import           Data.Traversable          (for, mapAccumL)
-import           Linear                    (V0(..), V1(..), V2(..))
-import           Safe                      (lastMay)
-import           Text.Read                 (readMaybe)
-import qualified Data.Conduit.Combinators  as C
-import qualified Data.Map                  as M
-import qualified Data.Sequence             as Seq
+import           AOC.Solver               ((:~>)(..))
+import           AOC.Util                 (maybeToEither, eitherToMaybe)
+import           Control.Monad            (void)
+import           Control.Monad.Except     (MonadError, throwError)
+import           Control.Monad.State      (MonadState, get, put, modify)
+import           Data.Conduit             (ConduitT, await, yield, (.|), runConduit)
+import           Data.Conduit.Lift        (runStateC)
+import           Data.List.Split          (splitOn)
+import           Data.Map                 (Map)
+import           Data.Sequence            (Seq)
+import           Data.Traversable         (for, mapAccumL)
+import           Linear                   (V1(..), V2(..))
+import           Safe                     (lastMay)
+import           Text.Read                (readMaybe)
+import qualified Data.Conduit.Combinators as C
+import qualified Data.Map                 as M
+import qualified Data.Sequence            as Seq
 
 data Memory = Mem
     { mPos  :: Int
@@ -49,43 +45,12 @@ data Mode = Pos | Imm
 data Instr = Add | Mul | Get | Put | Jnz | Jez | Clt | Ceq | Hlt
   deriving (Eq, Ord, Enum, Show)
 
--- readMem :: Memory -> Maybe (Int, Memory)
--- readMem (Mem p r) = (, Mem (p + 1) r) <$> Seq.lookup p r
-
--- seekMem :: Int -> Memory -> Memory
--- seekMem i (Mem _ r) = Mem i r
-
 instrMap :: Map Int Instr
 instrMap = M.fromList $
     (99, Hlt) : zip [1 ..] [Add ..]
 
 instr :: Int -> Maybe Instr
-instr = (`M.lookup` instrMap) . (`mod` 100)
-
-data InstrOp m = forall t. (Traversable t, Applicative t) => InstrOp (t Int -> ConduitT Int Int m InstrRes)
-
-data InstrRes = IRWrite Int
-              | IRJump  Int
-              | IRNop
-              | IRHalt
-  deriving Show
-
-instrOp
-    :: MonadError String m
-    => Instr
-    -> InstrOp m
-instrOp = \case
-    Add -> InstrOp $ \case V2 x y -> pure . IRWrite $ x + y
-    Mul -> InstrOp $ \case V2 x y -> pure . IRWrite $ x * y
-    Get -> InstrOp $ \case V0     -> await >>= \case
-                             Nothing -> throwError "no input";
-                             Just x  -> pure $ IRWrite x
-    Put -> InstrOp $ \case V1 x   -> IRNop <$ yield x
-    Jnz -> InstrOp $ \case V2 x y -> pure $ if x /= 0 then IRJump y else IRNop
-    Jez -> InstrOp $ \case V2 x y -> pure $ if x == 0 then IRJump y else IRNop
-    Clt -> InstrOp $ \case V2 x y -> pure . IRWrite $ if x <  y then 1 else 0
-    Ceq -> InstrOp $ \case V2 x y -> pure . IRWrite $ if x == y then 1 else 0
-    Hlt -> InstrOp $ \case V0     -> pure IRHalt
+instr = (`M.lookup` instrMap)
 
 readMem
     :: (MonadState Memory m, MonadError String m)
@@ -105,13 +70,15 @@ peekMem i = do
       Nothing -> throwError "bad index"
       Just x  -> pure x
 
-runInstrOp
-    :: (Traversable t, MonadError String m, MonadState Memory m)
-    => t Mode
-    -> (t Int -> m InstrRes)
-    -> m InstrRes
-runInstrOp modes f = do
-    inp <- for modes $ \mode -> do
+-- | Run a @t Int -> m r@ function by getting fetching an input container
+-- @t Int@.
+withInput
+    :: (Traversable t, Applicative t, MonadState Memory m, MonadError String m)
+    => Int      -- ^ mode int
+    -> (t Int -> m r)
+    -> m r
+withInput mo f = do
+    inp <- for (fillModes mo) $ \mode -> do
       a <- readMem
       case mode of
         Pos -> peekMem a
@@ -127,25 +94,40 @@ fillModes i = snd $ mapAccumL go i (pure ())
       where
         (t,o) = j `divMod` 10
 
+-- | Useful type to abstract over the actions of the different operations
+data InstrRes = IRWrite Int         -- ^ write a value
+              | IRJump  Int         -- ^ jump to position
+              | IRNop               -- ^ do nothing
+              | IRHalt              -- ^ halt
+  deriving Show
+
 step
     :: (MonadError String m, MonadState Memory m)
     => ConduitT Int Int m Bool
 step = do
-    x <- readMem
-    o <- maybeToEither "bad instr" $ instr x
-    case instrOp o of
-      InstrOp f -> do
-        ir <- runInstrOp (fillModes (x `div` 100)) f
-        case ir of
-          IRWrite y -> do
-            c <- readMem
-            True <$ modify (\(Mem p r) -> Mem p (Seq.update c y r))
-          IRJump  z ->
-            True <$ modify (\(Mem _ r) -> Mem z r)
-          IRNop     ->
-            pure True
-          IRHalt    ->
-            pure False
+    (mo, x) <- (`divMod` 100) <$> readMem
+    o  <- maybeToEither "bad instr" $ instr x
+    ir <- case o of
+      Add -> withInput mo $ \case V2 a b  -> pure . IRWrite $ a + b
+      Mul -> withInput mo $ \case V2 a b  -> pure . IRWrite $ a * b
+      Get -> await      >>= \case Nothing -> throwError "no input"
+                                  Just a  -> pure $ IRWrite a
+      Put -> withInput mo $ \case V1 a    -> IRNop <$ yield a
+      Jnz -> withInput mo $ \case V2 a b  -> pure $ if a /= 0 then IRJump b else IRNop
+      Jez -> withInput mo $ \case V2 a b  -> pure $ if a == 0 then IRJump b else IRNop
+      Clt -> withInput mo $ \case V2 a b  -> pure . IRWrite $ if a <  b then 1 else 0
+      Ceq -> withInput mo $ \case V2 a b  -> pure . IRWrite $ if a == b then 1 else 0
+      Hlt                                 -> pure IRHalt
+    case ir of
+      IRWrite y -> do
+        c <- readMem
+        True <$ modify (\(Mem p r) -> Mem p (Seq.update c y r))
+      IRJump  z ->
+        True <$ modify (\(Mem _ r) -> Mem z r)
+      IRNop     ->
+        pure True
+      IRHalt    ->
+        pure False
 
 runProg :: [Int] -> Memory -> Either String [Int]
 runProg inp m = runConduit $ C.yieldMany inp

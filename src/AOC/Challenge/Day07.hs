9372b84617842ebee56d4baa63361c8a06450e90
Justin Le <justin@jle.im>
2019-12-07 02:44:04 -0800

    day 7 cleanup more

--- src/AOC/Challenge/Day07.hs
+++ src/AOC/Challenge/Day07.hs
@@ -6,31 +6,20 @@
 -- Portability : non-portable
 --
 -- Day 7.  See "AOC.Solver" for the types used in this module!
---
--- After completing the challenge, it is recommended to:
---
--- *   Replace "AOC.Prelude" imports to specific modules (with explicit
---     imports) for readability.
--- *   Remove the @-Wno-unused-imports@ and @-Wno-unused-top-binds@
---     pragmas.
--- *   Replace the partial type signatures underscores in the solution
---     types @_ :~> _@ with the actual types of inputs and outputs of the
---     solution.  You can delete the type signatures completely and GHC
---     will recommend what should go in place of the underscores.
 
 module AOC.Challenge.Day07 (
     day07a
   , day07b
   ) where
 
-import           AOC.Common.Conduino       (evalStateP, feedbackP, runExceptP)
+import           AOC.Common.Conduino       (evalStateP, feedbackP)
 import           AOC.Common.Intcode        (Memory, VM, untilHalt, stepForeverAndDie, parseMem)
 import           AOC.Solver                ((:~>)(..))
 import           Control.Monad.Except      (MonadError, throwError)
 import           Data.Conduino             (Pipe, (.|), yield, runPipePure, runPipe, awaitSurely)
 import           Data.List                 (permutations)
 import           Data.Void                 (Void)
-import           Safe                      (lastMay, maximumMay)
+import           Safe                      (maximumMay)
 import qualified Data.Conduino.Combinators as C
 
 yieldAndDie :: MonadError String m => o -> Pipe i o u m a

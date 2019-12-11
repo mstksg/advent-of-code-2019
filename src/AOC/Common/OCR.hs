5e55c57a1c03a0015b94c6ddd1b4a19a07745b47
Justin Le <justin@jle.im>
2019-12-11 09:05:38 -0800

    more robust OCR"

--- src/AOC/Common/OCR.hs
+++ src/AOC/Common/OCR.hs
@@ -2,47 +2,73 @@
 
 module AOC.Common.OCR (
     parseLetters
+  , contiguousShapes
   ) where
 
 import           AOC.Common
+import           Control.Lens
 import           Control.Monad
 import           Data.Bifunctor
+import           Data.Foldable
 import           Data.List
-import           Data.Map       (Map)
+import           Data.Map          (Map)
+import           Data.Profunctor
 import           Data.Semigroup
-import           Data.Set       (Set)
+import           Data.Set          (Set)
+import           Data.Set.NonEmpty (NESet)
 import           Linear
-import           Text.Heredoc   (here)
-import qualified Data.Map       as M
-import qualified Data.Set       as S
+import           Text.Heredoc      (here)
+import qualified Control.Foldl     as F
+import qualified Data.Map          as M
+import qualified Data.Set          as S
+import qualified Data.Set.NonEmpty as NES
+
+-- | The set of unconnected shapes, indexed by their original center of
+-- mass
+contiguousShapes :: Set Point -> Map (V2 Double) (NESet Point)
+contiguousShapes s0 = M.fromList
+    [ (com, NES.map (subtract topCorner) s)
+    | NES.IsNonEmpty s <- S.toList . S.map flood $ s0
+    , let com            = F.fold ((lmap . fmap) fromIntegral F.mean) s
+          V2 topCorner _ = boundingBox s
+    ]
+  where
+    flood = floodFill (S.fromList . filter (`S.member` s0) . fullNeighbs)
+          . S.singleton
+
+-- | The set of unconnected shapes, sorted against some function on their
+-- original center of masses
+contiguousShapesBy
+    :: Ord a
+    => (V2 Double -> a)
+    -> Set Point
+    -> [NESet Point]
+contiguousShapesBy f = toList . M.mapKeys f . contiguousShapes
 
 parseLetters
     :: Set Point
     -> String
-parseLetters = map (\c -> M.findWithDefault '?' c letterMap) . unfoldr peel
+parseLetters = map (\c -> M.findWithDefault '?' c letterMap)
+             . toList
+             . contiguousShapesBy (view _x)
 
 -- | A map of a set of "on" points (for a 4x6 grid) to the letter they
 -- represent
-letterMap :: Map (Set Point) Char
+letterMap :: Map (NESet Point) Char
 letterMap = M.fromList
           . uncurry (zipWith (flip (,)))
-          . second (unfoldr peel . M.keysSet . parseAsciiMap (guard . (== '#')))
+          . second ( contiguousShapesBy (view _x)
+                   . M.keysSet
+                   . parseAsciiMap (guard . (== '#'))
+                   )
           $ rawLetterforms
 
-peel :: Set Point -> Maybe (Set Point, Set Point)
-peel ps = do
-    Min xMin <- flip foldMap ps $ \(V2 x _) -> Just (Min x)
-    let ps' = subtract (V2 xMin 0) `S.map` ps
-    pure $ S.partition (\(V2 x _) -> x < 4) ps'
-
-
--- | don't include y since it is a weird length...
 rawLetterforms :: (String, String)
-rawLetterforms = ("ABCEFGHJKLPRUZ", drop 1 [here|
-.##.###..##.########.##.#..#..###..##...###.###.#..#####
-#..##..##..##...#...#..##..#...##.#.#...#..##..##..#...#
-#..####.#...###.###.#...####...###..#...#..##..##..#..#.
-#####..##...#...#...#.###..#...##.#.#...###.###.#..#.#..
-#..##..##..##...#...#..##..##..##.#.#...#...#.#.#..##...
-#..####..##.#####....####..#.##.#..######...#..#.##.####
+rawLetterforms = ("ABCEFGHJKLPRUYZ", drop 1 [here|
+.##..###...##..####.####..##..#..#...##.#..#.#....###..###..#..#.#...#.####
+#..#.#..#.#..#.#....#....#..#.#..#....#.#.#..#....#..#.#..#.#..#.#...#....#
+#..#.###..#....###..###..#....####....#.##...#....#..#.#..#.#..#..#.#....#.
+####.#..#.#....#....#....#.##.#..#....#.#.#..#....###..###..#..#...#....#..
+#..#.#..#.#..#.#....#....#..#.#..#.#..#.#.#..#....#....#.#..#..#...#...#...
+#..#.###...##..####.#.....###.#..#..##..#..#.####.#....#..#..##....#...####
 |])
